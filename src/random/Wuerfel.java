e9276f210ac4699585c3a503e5eeaa844218321b
Barne Kleinen <barne.kleinen@htw-berlin.de>
2021-12-01 14:47:34 +0100

    litte amendmends

--- src/random/Wuerfel.java
+++ src/random/Wuerfel.java
@@ -1,7 +1,7 @@
 package random;
 
 public class Wuerfel {
-    public static void main(String[] a){
-        System.out.println(((int)(Math.random()*6)) + 1);
+    public static void main(String[] a) {
+        System.out.println(((int) (Math.random() * 6)) + 1);
     }
 }
--- src/seed/RandomDemo.java
+++ src/seed/RandomDemo.java
@@ -1,18 +1,20 @@
 package seed;
+
 import java.util.Random;
 
 public class RandomDemo {
 
-	public static void main(String[] args) {
-		for (int j = 0; j < 10; j++) {
-			Random r = new Random(2307824);
+    public static void main(String[] args) {
+		System.out.println("with a fixed seed, the sequence of pseudorandom \nnumbers is always the same:");
+        for (int j = 0; j < 10; j++) {
+            Random r = new Random(2307824);
 
-			for (int i = 0; i < 6; i++) {
-				System.out.print("" + (r.nextInt(6) + 1) + ", ");
-			}
-			System.out.println();
-		}
+            for (int i = 0; i < 6; i++) {
+                System.out.print("" + (r.nextInt(6) + 1) + ", ");
+            }
+            System.out.println();
+        }
 
-	}
+    }
 
 }
